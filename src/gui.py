88c9f5ce1b72f1fd9474d090c875cc0966dfd9da
Jack477 <47400151+Jack477@users.noreply.github.com>
2020-05-09 11:07:32 +0200

    Update gui.py
    Check entry fields

--- src/gui.py
+++ src/gui.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import resources as rs
 import tkinter as tk
+import importlib
 from tkinter import messagebox as msb
 from tkinter import *
 
@@ -43,12 +44,10 @@ class Info_Window:
 		
 		master.mainloop()
 		
-		
+	
 class Overclock_Window:
-		
+
 	def __init__(master):
-		
-		
 		master = tk.Tk()
 		master.geometry("300x400")
 		master.title("Commander Pi")
@@ -59,19 +58,19 @@ class Overclock_Window:
 		arm_freq_entry = tk.Entry(master)
 		arm_freq_entry.pack(fill=X)
 		
-		arm_freq_b = tk.Button ( master, text="Set arm_freq", command = lambda:overclock_push(arm_freq_entry))
+		arm_freq_b = tk.Button ( master, text="Set arm_freq", command = lambda:overclock_push(arm_freq_entry, 1))
 		arm_freq_b.pack(fill=X)
 		
 		gpu_freq_entry = tk.Entry(master)
 		gpu_freq_entry.pack(fill=X)
 		
-		gpu_freq_b = tk.Button ( master, text="Set gpu_freq", command = lambda:overclock_push(gpu_freq_entry))
+		gpu_freq_b = tk.Button ( master, text="Set gpu_freq", command = lambda:overclock_push(gpu_freq_entry, 2))
 		gpu_freq_b.pack(fill=X)
 		
 		over_voltage_entry = tk.Entry(master)
 		over_voltage_entry.pack(fill=X)
 		
-		over_voltage_b = tk.Button ( master, text="Set over_voltage", command = lambda:overclock_push(over_voltage_entry))
+		over_voltage_b = tk.Button ( master, text="Set over_voltage", command = lambda:overclock_push(over_voltage_entry, 3))
 		over_voltage_b.pack(fill=X)
 		
 		reboot_b = tk.Button (master, text="Apply and Reboot", command = lambda:confirum_push())
@@ -80,7 +79,8 @@ class Overclock_Window:
 		proposition = tk.Label( master, text="Stable proposed values:\narm_freq=2000\ngpu_freq=400\nover_voltage=6\nMax level:\narm_freq=2147\ngpu_freq=750\nover_voltage=6", fg="red" )
 		proposition.pack(fill=X)
 
-		def overclock_push(entry_stuff):
+		def overclock_push(entry_stuff, state):
+			rs.set_push_state(state)
 			entry_stuff.config(state='disabled')
 		#def overclock_push2(entry_stuff):
 		#	global overclock_push_state2
@@ -92,9 +92,12 @@ class Overclock_Window:
 		#	entry_stuff.config(state='disabled')
 			
 		def confirum_push():
+			print(rs.push_state1)
+			print(rs.push_state2)
+			print(rs.push_state3)
 			confirm_msgb = msb.askyesno(title=None, message="Are you sure?")
 			if confirm_msgb == True:
-				if arm_freq_entry.get() != "" and gpu_freq_entry.get() != "" and over_voltage_entry.get() != "":
+				if arm_freq_entry.get() != "" and gpu_freq_entry.get() != "" and over_voltage_entry.get() != "" and rs.push_state1 == True and rs.push_state2 == True and rs.push_state3 == True:
 					rs.overclock_arm_freq(arm_freq_entry.get())
 					rs.overclock_gpu_freq(gpu_freq_entry.get())
 					rs.overclock_over_voltage(over_voltage_entry.get())
@@ -103,6 +106,7 @@ class Overclock_Window:
 				else:
 					msb.showinfo(title="Warning", message="You don't set all values!")
 			else:
+				importlib.reload(rs)
 				master.destroy()
 				
 				
