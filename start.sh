61a1b87d801d56344ce91a4ab6fb4888fb19429f
Jack477 <47400151+Jack477@users.noreply.github.com>
2020-05-09 12:20:46 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-sudo python3 src/main.py
+
+problem=$(dpkg -s python3-tk|grep installed)
+echo Checking for tkinter: $problem
+if [ "" == "$problem" ]; then
+    sudo apt-get install python3-tk
+	sudo python3 src/main.py
+else
+	sudo python3 src/main.py
+fi
 
