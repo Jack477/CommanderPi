993e2ca108573f5fb7548007a51dfc7aaa5f4ead
Jack477 <47400151+Jack477@users.noreply.github.com>
2020-05-19 16:50:47 +0200

    Add files via upload
    Update Commander Pi to 0.2 version :)

--- src/start.sh
+++ src/start.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
+sudo python3 /home/pi/Commander_Pi/src/main.py > a.txt
 
-problem=$(dpkg -s python3-tk|grep installed)
-echo Checking for tkinter: $problem
-if [ "" == "$problem" ]; then
-    sudo apt-get install python3-tk
-	sudo python3 main.py
-else
-	sudo python3 main.py
-fi
 
 
