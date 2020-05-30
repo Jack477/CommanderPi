b6bf568859b17c4c9c847b4f2f4759414d7d48c7
Jack477 <47400151+Jack477@users.noreply.github.com>
2020-05-30 08:32:54 +0200

    Update update.py
    Added deleting old files def

--- src/update.py
+++ src/update.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python
 import urllib.request
+import os
+path = os.path.dirname(os.path.realpath(__file__))
 Files = ["https://raw.githubusercontent.com/Jack477/CommanderPi/master/src/gui.py", "https://raw.githubusercontent.com/Jack477/CommanderPi/master/src/resources.py", "https://raw.githubusercontent.com/Jack477/CommanderPi/master/src/bootloader.py", "https://raw.githubusercontent.com/Jack477/CommanderPi/master/src/main.py"]
 Names = ["gui.py", "resources.py", "bootloader.py", "main.py"]
+def delete_old():
+	for name in Names:
+	os.system('rm '+path+'/'+name)
 def download_git(url, name):
-	filename, headers = urllib.request.urlretrieve(url, filename="/home/pi/CommanderPi/src/"+name)
+	filename, headers = urllib.request.urlretrieve(url, filename="/home/pi/Commander_Pi/src/"+name)
 	print ("download start!")
 	print ("download complete!")
 	print ("download file location: ", filename)
 	print ("download headers: ", headers)
 def update_cpi():
+	delete_old()
 	for f, x in zip(Files, Names):
 		download_git(f, x)
 
