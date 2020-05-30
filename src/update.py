139d587a45256be54e9b716f6cf2b7834686e7fd
Jack477 <47400151+Jack477@users.noreply.github.com>
2020-05-30 09:21:35 +0200

    Update update.py

--- src/update.py
+++ src/update.py
@@ -7,14 +7,14 @@ Names = ["gui.py", "resources.py", "bootloader.py", "main.py"]
 def delete_old():
 	for name in Names:
 		os.system('rm '+path+'/'+name)
+		print("Deleted "+name)
 def download_git(url, name):
 	filename, headers = urllib.request.urlretrieve(url, filename="/home/pi/Commander_Pi/src/"+name)
 	print ("download start!")
 	print ("download complete!")
 	print ("download file location: ", filename)
-	print ("download headers: ", headers)
+	#print ("download headers: ", headers)
 def update_cpi():
 	delete_old()
 	for f, x in zip(Files, Names):
 		download_git(f, x)
-
