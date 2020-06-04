e234fd8cef8dc07db3b26013e7884225a7ecb821
Jack477 <47400151+Jack477@users.noreply.github.com>
2020-06-05 00:18:20 +0200

    Rename desktop file

--- c_desktop.py
+++ c_desktop.py
@@ -2,9 +2,9 @@
 import os 
 import subprocess as sp
 dir_path = os.path.dirname(os.path.realpath(__file__))
-f_content = '[Desktop Entry]\nName=CommanderPi\nComment=System info and overclocking\nExec=/home/pi/CommanderPi/src/start.sh\nIcon=/home/pi/CommanderPi/src/logo.png\nCategories=Utility;\nVersion=1.0\nType=Application\nTerminal=false\nStartupNotify=true'
-d_dir = "/home/pi/Desktop/test.desktop"
-x_dir = "/usr/share/applications/test.desktop"
+f_content = '[Desktop Entry]\nName=CommanderPi\nComment=System info and overclocking\nExec=/home/pi/CommanderPi/src/start.sh\nIcon=/home/pi/CommanderPi/src/icon.png\nCategories=Utility;\nVersion=1.0\nType=Application\nTerminal=false\nStartupNotify=true'
+d_dir = "/home/pi/Desktop/commanderpi.desktop"
+x_dir = "/usr/share/applications/commanderpi.desktop"
 print(d_dir)
 f = open(d_dir, "w+")
 f.write(f_content)
