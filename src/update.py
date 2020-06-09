291fb365609f60d362b2bf187efd4ace1b3c4986
Jack477 <47400151+Jack477@users.noreply.github.com>
2020-06-09 15:34:49 +0200

    Update update.py
    Add update_alert() <still in build>

--- src/update.py
+++ src/update.py
@@ -1,6 +1,19 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import urllib.request
 import os
+import resources as rs
+
+#def update_alert():
+#	git_version=""
+#	data = ""
+#	data = urllib.request.urlopen('https://raw.githubusercontent.com/Jack477/CommanderPi/master/src/update.py')
+#	for line in data.readlines():
+#		print(line)
+#		if "app_version" in line:
+#			git_version = line
+#	print(git_version)
+
+	#print(git_version)
 path = os.path.dirname(os.path.realpath(__file__))
 Files = ["https://raw.githubusercontent.com/Jack477/CommanderPi/master/src/gui.py", "https://raw.githubusercontent.com/Jack477/CommanderPi/master/src/resources.py", "https://raw.githubusercontent.com/Jack477/CommanderPi/master/src/bootloader.py", "https://raw.githubusercontent.com/Jack477/CommanderPi/master/src/main.py"]
 Names = ["gui.py", "resources.py", "bootloader.py", "main.py"]
