b7cc8591f00f154a87ac7aabe5001985c6a10b20
Jack477 <47400151+Jack477@users.noreply.github.com>
2020-07-14 09:03:13 +0200

    Set relative path

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ if [ "" == "$problem" ]; then
 	sudo apt-get install python3-tk
 fi
 sudo apt-get install python3-pil python3-pil.imagetk
-sudo python3 c_desktop.py
-sudo chmod +x $path/src/start.sh
-sudo chmod +x /home/pi/Desktop/commanderpi.desktop
+sudo python3 c_desktop.py ${HOME}
+sudo chmod +x ${HOME}/CommanderPi/src/start.sh
+sudo chmod +x ${HOME}/Desktop/commanderpi.desktop
