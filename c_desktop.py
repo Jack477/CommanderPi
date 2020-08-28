8e43bdc6ccf2fe91adbc7c317780d442c2f15718
Jack477 <47400151+Jack477@users.noreply.github.com>
2020-08-28 16:58:42 +0200

    Fixed path issue

--- c_desktop.py
+++ c_desktop.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python
 import sys
-import os 
+import os
 import subprocess as sp
-home_path = sys.argv[1]
+from os.path import expanduser
+
+user = sys.argv[1]
+home_path = expanduser("~%s" % user)
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
-f_content = "[Desktop Entry]\nName=CommanderPi\nComment=System info and overclocking\nExec="+home_path+"/CommanderPi/src/start.sh\nIcon="+home_path+"/CommanderPi/src/icon.png\nCategories=Utility;\nVersion=1.0\nType=Application\nTerminal=false\nStartupNotify=true"
+
+print(dir_path)
+f_content = "[Desktop Entry]\nName=CommanderPi\nComment=System info and overclocking\nExec="+dir_path+"/src/start.sh\nIcon="+dir_path+"/src/icon.png\nCategories=Utility;\nVersion=1.0\nType=Application\nTerminal=false\nStartupNotify=true"
+print(f_content)
+
 d_dir = home_path+"/Desktop/commanderpi.desktop"
 x_dir = "/usr/share/applications/commanderpi.desktop"
-print(d_dir)
+
+print("Save desktop shortcut to %s" % d_dir)
 f = open(d_dir, "w+")
 f.write(f_content)
 f.close
+
+print("Save menu shortcut to %s" % x_dir)
 f2 = open(x_dir, "w+")
 f2.write(f_content)
 f2.close
--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ if [ "" == "$problem" ]; then
 	sudo apt-get install python3-tk
 fi
 sudo apt-get install python3-pil python3-pil.imagetk
-sudo python3 c_desktop.py ${HOME}
-sudo chmod +x ${HOME}/CommanderPi/src/start.sh
+sudo python3 c_desktop.py $USER
+sudo chmod +x ${path}/src/start.sh
 sudo chmod +x ${HOME}/Desktop/commanderpi.desktop
