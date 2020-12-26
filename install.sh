aa3eb0937662a7c33c9ff2f4a8d205ce25bb2116
Jack477 <47400151+Jack477@users.noreply.github.com>
2020-12-26 20:33:23 +0100

    Added psutil install by pip

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ echo Checking for tkinter: $problem
 if [ "" == "$problem" ]; then
 	sudo apt-get install python3-tk
 fi
+sudo pip install psutil
 sudo apt-get install python3-pil python3-pil.imagetk
 sudo python3 c_desktop.py $USER
 sudo chmod +x ${path}/src/start.sh
