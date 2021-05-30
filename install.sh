ce24c4cdbfacb21254dd9fa1093da25bd65a16c6
Jack477 <kubalegol2012@wp.pl>
2021-05-30 16:01:53 +0200

    changed desktop file owner

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ echo Checking for tkinter: $problem
 sudo pip3 install psutil
 sudo apt-get install git python3-tk python3-pil python3-pil.imagetk
 sudo python3 c_desktop.py $USER
+sudo chown $USER ~/Desktop/commanderpi.desktop
 sudo chmod +x ${path}/src/start.sh
 sudo chmod +x ${HOME}/Desktop/commanderpi.desktop
