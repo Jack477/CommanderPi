4673c87a781293b2f390aba7ebb62ba3f25c61f5
Jack477 <kubalegol2012@wp.pl>
2021-09-09 17:47:00 +0200

    Checking for Desktop directory before run c_desktop.py

--- install.sh
+++ install.sh
@@ -5,7 +5,12 @@ echo $path
 echo Checking for tkinter: $problem
 sudo pip3 install psutil
 sudo apt-get install git python3-tk python3-pil python3-pil.imagetk
-sudo python3 c_desktop.py $USER
-sudo chown $USER ~/Desktop/commanderpi.desktop
 sudo chmod +x ${path}/src/start.sh
-sudo chmod +x ${HOME}/Desktop/commanderpi.desktop
+if [ -d "$HOME/Desktop" ]
+then
+    sudo python3 c_desktop.py $USER
+    sudo chown $USER ~/Desktop/commanderpi.desktop
+    sudo chmod +x ${HOME}/Desktop/commanderpi.desktop
+else
+    echo "Could not find Desktop path!"
+fi
