3d6c579b7090022322472cded7ed27a0ffa1d992
Jack477 <kubalegol2012@wp.pl>
2021-09-09 18:21:02 +0200

    Moved c_desktop.py run

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ echo Checking for tkinter: $problem
 sudo pip3 install psutil
 sudo apt-get install git python3-tk python3-pil python3-pil.imagetk
 sudo chmod +x ${path}/src/start.sh
+sudo python3 c_desktop.py $USER
 if [ -d "$HOME/Desktop" ]
 then
-    sudo python3 c_desktop.py $USER
     sudo chown $USER ~/Desktop/commanderpi.desktop
     sudo chmod +x ${HOME}/Desktop/commanderpi.desktop
 else
